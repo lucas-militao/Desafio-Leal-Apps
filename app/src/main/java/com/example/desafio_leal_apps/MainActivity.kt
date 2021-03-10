bbe96aa09f75aea4124b9a4159ae9ba196be648c
lumilitas <lucas23121995>
2021-03-10 16:36:13 -0300

    Login realizado com sucesso

--- app/src/main/java/com/example/desafio_leal_apps/MainActivity.kt
+++ app/src/main/java/com/example/desafio_leal_apps/MainActivity.kt
@@ -1,30 +1,60 @@
 package com.example.desafio_leal_apps
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var emailTextField: TextInputEditText
+    private lateinit var passwordTextField: TextInputEditText
+
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        // ...
-        // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
 
+        setupView()
     }
 
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-            print("Hello DEU CERTO!!!!!!!!!!!!!!!!!!!!")
-        }
+    private fun setupView() {
+        emailTextField = findViewById(R.id.emailTextField)
+        passwordTextField = findViewById(R.id.passwordTextField)
+
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener { login(
+                email = emailTextField.text.toString(),
+                password = passwordTextField.text.toString()
+        ) }
+    }
+
+    @SuppressLint("ShowToast")
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("TAG", "signInWithEmail:success")
+                        val user = auth.currentUser
+                        Toast.makeText(applicationContext, "O usuário foi logado", Toast.LENGTH_LONG).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("TAG", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(applicationContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        // ...
+                    }
+                    // ...
+                }
     }
 }
