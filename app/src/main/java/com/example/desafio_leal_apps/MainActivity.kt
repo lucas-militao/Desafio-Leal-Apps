bd3bd26b97690f40c2c5fe1dbb64c3a4c36ef795
lumilitas <lucas23121995>
2021-03-10 09:40:12 -0300

    Testando conexão com firebase

--- app/src/main/java/com/example/desafio_leal_apps/MainActivity.kt
+++ app/src/main/java/com/example/desafio_leal_apps/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.desafio_leal_apps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // ...
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            print("Hello DEU CERTO!!!!!!!!!!!!!!!!!!!!")
+        }
     }
 }
